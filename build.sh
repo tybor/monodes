0491f07005eff7885a6641756b95df155b4a398a
tybor <paolo.redaelli@gmail.com>
2011-04-17 01:07:23 +0200

    Symbian build script, mostly working

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+export PATH=/home/paolo/gnupoc/bin:/home/paolo/gnupoc/qt_4.7.0/bin:$PATH
+export EPOCROOT=/home/paolo/gnupoc/sdk_5.0/ # trailing "/" is required!
+unset  QMAKESPEC    # make sure there is no QMAKESPEC variable set
+
+#I wasn't able to make "qmake gnupoc" produce correct Makefiles for Symbian.
+#For now just use qmake from QtCreator
+make distclean 
+
+make release-gcce 
+make sis QT_SIS_CERTIFICATE=OVI_06-28-2010.crt QT_SIS_KEY=OVI_06-28-2010.key
+make installer_sis QT_SIS_CERTIFICATE=OVI_06-28-2010.crt QT_SIS_KEY=OVI_06-28-2010.key
+
